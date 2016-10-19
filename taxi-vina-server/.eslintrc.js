7c80713f5cbd8a3a5c792bcb406b0c88f0c292c5
Quang Minh NGUYEN <quangminhnguyen@MacBook-de-KidTy.local>
2016-10-19 13:23:44 +0200

    Added .gitignore file

---
+++ taxi-vina-server/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    extends: 'usecases/usecase/nodejs',
+    "rules": {
+        "no-console":0
+      }
+        /*"indent": [
+            2,
+            "tab"
+        ],
+        "quotes": [
+            2,
+            "single"
+        ],
+        "linebreak-style": [
+            2,
+            "unix"
+        ],
+        "semi": [
+            2,
+            "always"
+        ]
+    },
+    "env": {
+        "browser": true,
+        "node": true
+    },
+    "extends": "eslint:recommended"*/
+
+
+
+};
